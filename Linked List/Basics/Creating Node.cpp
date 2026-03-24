38674aa8289a967b1fadbd590b764d082643c0be
Hasan Ahmmad <mdhasanahmmad335@gmail.com>
2026-03-25 03:03:14 +0600

    Create Creating Node.cpp

---
+++ Linked List/Basics/Creating Node.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node{
+
+public:
+    int val;
+    Node* next;
+
+    Node(int val ){  //Node constructor
+        this->val=val;
+        this->next=NULL;
+    }
+};
+
+
+int main(){
+
+    Node* head = new Node(10);  // Creating dynamic node 
+    Node* a = new Node(20);
+    Node* b = new Node(30);
+    Node* c = new Node(40);
+    Node* d = new Node(50);
+
+    head->next=a;
+    a->next=b;
+    b->next=c;
+    c->next=d;
+
+    cout<<head->val<<endl;
+    cout<<head->next->val<<endl; // cout<<a->val<<endl;
+    cout<<head->next->next->val<<endl; //cout<<b->val<<endl;
+    
+
+}   
