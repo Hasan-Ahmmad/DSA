b6546c45a4d2cde78437ace6d8cb0af2ccfb0dfc
Hasan Ahmmad <mdhasanahmmad335@gmail.com>
2026-03-25 14:45:29 +0600

    Create Insert At Tail Optimized.cpp

---
+++ Linked List/Basics/Insert At Tail Optimized.cpp
@@ -0,0 +1,55 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node{
+
+public:
+    int val;
+    Node* next;
+
+    Node(int val ){
+        this->val=val;
+        this->next=NULL;
+    }
+};
+
+void insert_at_tail(Node* &head, Node* &tail, int val){  //O(1)
+
+    Node* newnode= new Node(val);
+    if(head==NULL){
+        head=newnode;
+        tail=newnode;
+    }
+    tail->next=newnode;
+    tail=newnode;
+}
+
+void print_list(Node* head){
+
+    Node* temp=head;
+
+    while( temp != NULL){
+
+        cout<<temp->val<<endl;
+        temp=temp->next;
+    }
+}
+
+int main(){
+
+    Node* head = new Node(10);
+    Node* a = new Node(20);
+    Node* b = new Node(30);
+    Node* c = new Node(40);
+    Node* tail = new Node(50);
+
+    head->next=a;
+    a->next=b;
+    b->next=c;
+    c->next=tail;
+
+    insert_at_tail(head,tail, 100);
+    
+    print_list(head);
+
+}   
