8d3ad92be4abac30d7127aa39a64702c1f2efa7f
Hasan Ahmmad <mdhasanahmmad335@gmail.com>
2026-03-27 16:24:39 +0600

    Create Doubly Insert At Head.cpp

---
+++ Linked List/Basics/Doubly Insert At Head.cpp
@@ -0,0 +1,54 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node{
+
+public:
+    int val;
+    Node* next;
+    Node* prev;
+
+    Node(int val){
+        this->val=val;
+        this->next=NULL;
+        this->prev=NULL;
+    }
+};
+
+void Print_forward(Node* head){
+
+    Node* tmp=head;
+    while(tmp !=NULL){
+        cout<<tmp->val<<" ";
+        tmp=tmp->next;
+    }
+    cout<<endl;
+}
+
+void insert_at_head(Node* &head){
+
+    Node* newnode = new Node(500);
+    newnode->next=head;
+    head->prev=newnode;
+    head=newnode;
+}
+
+int main(){
+
+    Node* head = new Node(10);
+    Node* a= new Node(20);
+    Node* b= new Node(30);
+    Node* tail =new Node(40);
+
+    head->next=a;
+    a->prev=head;
+    a->next=b;
+    b->prev=a;
+    b->next=tail;
+    tail->prev=b; 
+
+    insert_at_head(head);
+
+    Print_forward(head);
+
+}
