a18220e8f61e9b237d057150d8232438e1de3a1f
Hasan Ahmmad <mdhasanahmmad335@gmail.com>
2026-07-23 20:44:58 +0600

    Update First non repeating char.cpp

--- String/Basics/First non repeating char.cpp
+++ String/Basics/First non repeating char.cpp
@@ -15,3 +15,37 @@ int main(){
     }
 
 }
+
+
+
+//------------using frequency & unordered map-------------------O(n)
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    string s;
+    cin>>s;
+
+    vector<int> freq(256,0); // or unordered_map<char, int> freq;
+
+    for(char c : s){
+        freq[c]++;
+    }
+
+    bool flag = false;
+    char ans;
+    for(char c : s){
+        if(freq[c] == 1){
+            ans = c;
+            flag = true;
+            break;            
+        }
+    }
+    if(flag) cout<<ans<<endl;
+    else cout<<"Not found";
+
+}
+
+
