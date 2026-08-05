3039aabb2a5c97380a5b3e2af5583324f10a6d49
Hasan Ahmmad <mdhasanahmmad335@gmail.com>
2026-08-05 11:35:40 +0600

    Create Array Right Rotate by k.cpp

---
+++ Array/Basics/Array Right Rotate by k.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    vector<int> v{1,2,3,4,5,6,7,8,9,10};
+
+    int k = 2;
+
+    reverse(v.begin(), v.end());
+
+    reverse(v.begin(), v.begin()+k);
+
+    reverse(v.begin()+k, v.end());
+
+    for(auto i : v){
+        cout<< i<<" ";
+    }
+    cout<< endl;
+
+}   
