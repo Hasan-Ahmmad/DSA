68cad00f9e26de2ad2e4b5728c6b9d5ce5920978
Hasan Ahmmad <mdhasanahmmad335@gmail.com>
2026-08-07 16:06:40 +0600

    Create Prefix Sum.cpp

---
+++ Array/Basics/Prefix Sum.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    int n; cin>>n;
+    vector<int> v(n);
+
+    for(int i=0; i<n; i++) cin>>v[i];
+
+    vector<int> preSum(n);
+    preSum[0] = v[0];
+
+    for(int i=1; i<n; i++){
+        preSum[i] = preSum[i-1] + v[i];
+    }
+
+    for(int i : preSum){
+        cout<< i <<" ";
+    }
+    cout<<endl;
+
+}   
